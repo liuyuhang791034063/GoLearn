a33593c10fc928d6cddf7a4711e5d9ebf862f1ec
liuyuhang791034063 <liuyuhang791034063@qq.com>
2020-10-26 23:06:45 +0800

    add chat code

---
+++ ch8/chat/chat.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+	}
+}
